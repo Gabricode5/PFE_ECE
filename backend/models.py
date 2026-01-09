1acca42964fc6efa3922cdf6baee237d1523b4ff
Gabricode5 <gabriel.guery10@gmail.com>
2026-01-09 18:10:58 +0100

    création des class pour mes tables

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,8 @@
-from sqlalchemy import Column, Integer, String, Text, DateTime
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey, CheckConstraint
 from sqlalchemy.sql import func
-from database import Base # On suppose que Base est défini dans database.py
+from sqlalchemy.orm import relationship
+from pgvector.sqlalchemy import Vector
+from database import Base 
 
 class User(Base):
     __tablename__ = "utilisateur"
@@ -12,4 +14,21 @@ class User(Base):
     prenom = Column(String(50))
     nom = Column(String(50))
     role = Column(String(20), server_default="user")
+    date_creation = Column(DateTime(timezone=True), server_default=func.now())
+
+class ChatSession(Base):
+    __tablename__ = "chat_session"
+
+    id = Column(Integer, primary_key=True, index=True)
+    id_utilisateur = Column(Integer, ForeignKey("utilisateur.id"), nullable=False)
+    title = Column(String(255), nullable=False)
+    date_creation = Column(DateTime(timezone=True), server_default=func.now())
+
+class Message(Base):
+    __tablename__ = "chat_message"
+
+    id = Column(Integer, primary_key=True, index=True)
+    id_chat_session = Column(Integer, ForeignKey("chat_session.id"), nullable=False)
+    role = Column(String(20), CheckConstraint("role IN ('user', 'assistant', 'system')"), nullable=False)
+    content = Column(Text, nullable=False)
     date_creation = Column(DateTime(timezone=True), server_default=func.now())
