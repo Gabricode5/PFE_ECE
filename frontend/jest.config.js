0ad196c49ec1553d1adcbb33b6004361ea436dce
guerygabriel <gabriel.guery10@gmail.com>
2026-04-28 22:04:09 +0200

    modification

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,6 +1,5 @@
-import type { Config } from "jest";
-
-const config: Config = {
+/** @type {import('jest').Config} */
+const config = {
   testEnvironment: "jsdom",
   transform: {
     "^.+\\.tsx?$": ["ts-jest", { tsconfig: { jsx: "react-jsx" } }],
@@ -10,4 +9,4 @@ const config: Config = {
   },
 };
 
-export default config;
+module.exports = config;
